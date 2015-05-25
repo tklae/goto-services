643cf1468501a40f1a209d15523b5129e26eb64d
tobi vogel <tvogel@thoughtworks.com>
2015-05-25 23:32:17 +0200

    add precise copy statement for fat.jar

--- src/dist/scripts/deploy.sh
+++ src/dist/scripts/deploy.sh
@@ -4,7 +4,7 @@ GO_PIPELINE_NAME=${1-local}
 
 echo "copy files for dist.zip"
 cp ./src/dist/config/dev.yml config-$GO_PIPELINE_NAME.yml
-cp ./build/libs/*.jar app-$GO_PIPELINE_NAME.jar
+cp ./build/libs/$GO_PIPELINE_NAME-fat.jar app-$GO_PIPELINE_NAME.jar
 
 echo "create dist.zip"
 zip dist-$GO_PIPELINE_NAME.zip config-$GO_PIPELINE_NAME.yml app-$GO_PIPELINE_NAME.jar
