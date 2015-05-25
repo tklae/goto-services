de5f7ee1e31a149f8a1e5df04b826815ad3c3f99
tobi vogel <tvogel@thoughtworks.com>
2015-05-25 22:56:55 +0200

    adapt deploy and run scripts to current vagrant setup

--- src/dist/scripts/deploy.sh
+++ src/dist/scripts/deploy.sh
@@ -11,3 +11,4 @@ echo "create dist.zip"
 zip dist-$GO_PIPELINE_NAME.zip config-$GO_PIPELINE_NAME.yml app-$GO_PIPELINE_NAME.jar
 
 echo "deploy dist.zip (wherever)"
+scp dist-$GO_PIPELINE_NAME.zip vagrant@12.12.12.12:/prod
--- src/dist/scripts/run.sh
+++ src/dist/scripts/run.sh
@@ -4,6 +4,8 @@ LIB_DIR=`pwd`
 source $LIB_DIR/common.sh
 
 echo "fetch dist.zip (from wherever)"
+ssh vagrant@12.12.12.12
+cd /prod
 
 echo "unzip dist"
 unzip dist-$GO_PIPELINE_NAME.zip -d $GO_PIPELINE_NAME
