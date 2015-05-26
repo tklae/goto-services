ea0e710ca13ffbb8c89ebf9a9039e16c76e0b289
tobi vogel <tvogel@thoughtworks.com>
2015-05-26 23:27:13 +0200

    fixing deploy script - scp copy with identityfile still open

--- src/dist/scripts/deploy.sh
+++ src/dist/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GO_PIPELINE_NAME=${1-local}
+GO_PIPELINE_NAME=${GO_PIPELINE_NAME-local}
 
 echo "copy files for dist.zip"
 cp ./src/dist/config/dev.yml config-$GO_PIPELINE_NAME.yml
@@ -10,4 +10,5 @@ echo "create dist.zip"
 zip dist-$GO_PIPELINE_NAME.zip config-$GO_PIPELINE_NAME.yml app-$GO_PIPELINE_NAME.jar
 
 echo "deploy dist.zip (wherever)"
+# need an ssh key
 scp dist-$GO_PIPELINE_NAME.zip vagrant@12.12.12.12:/prod
