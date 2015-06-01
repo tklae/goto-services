2ea85ee2cbd1934aee5b59704ad1c8c1622cf8dc
tobi vogel <tvogel@thoughtworks.com>
2015-06-01 22:06:11 +0200

    specify HOST env-variable to check health of other environments as well

--- src/dist/scripts/healthcheck.sh
+++ src/dist/scripts/healthcheck.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+HOST=${HOST-localhost:8093}
+
 status="fail"
-  max_checks=10
+max_checks=10
 
-  set +e
-  for i in `seq 1 $max_checks`; do
-    echo "${i}. try"
+set +e
+for i in `seq 1 $max_checks`; do
+  echo "${i}. try"
 
-    out=`curl -fsS --max-time 5 "http://localhost:8093/healthcheck"`
+  out=`curl -fsS --max-time 5 "http://$HOST/healthcheck"`
 
-    if [ $? -eq 0 ]; then
-      status="success"
-      echo $status
-      break
-    else
-      sleep 1
-    fi
-  done
-  set -e
-
-  if [ "${status}" != "success" ]; then
-    echo "healthcheck failed"
-    exit 1
+  if [ $? -eq 0 ]; then
+    status="success"
+    echo $status
+    break
+  else
+    sleep 1
   fi
+done
+set -e
+
+if [ "${status}" != "success" ]; then
+  echo "healthcheck failed"
+  exit 1
+fi
