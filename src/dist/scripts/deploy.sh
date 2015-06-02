abbc576111f292c2b89ee017ffa943005c1b9fec
tobi vogel <tvogel@thoughtworks.com>
2015-06-02 22:01:15 +0200

    set executable flag before making .zip file

--- src/dist/scripts/deploy.sh
+++ src/dist/scripts/deploy.sh
@@ -7,6 +7,7 @@ STAGE=$(echo $STAGE | tr '[A-Z]' '[a-z]')
 DEPLOY_DIR="vagrant@12.12.12.12:/$STAGE"
 
 echo "create dist-$SERVICE_NAME.zip"
+chmod +x ./dist/scripts/*.sh
 zip dist-$SERVICE_NAME.zip ./dist
 
 echo "deploy dist-$SERVICE_NAME.zip on stage $STAGE"
