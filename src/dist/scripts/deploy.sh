cd13a1669bcf6d238627e1ed220c0071cf82c44e
tobi vogel <tvogel@thoughtworks.com>
2015-06-02 22:59:01 +0200

    refer to newly created /environment folders

--- src/dist/scripts/deploy.sh
+++ src/dist/scripts/deploy.sh
@@ -4,11 +4,12 @@ SERVICE_NAME=${SERVICE_NAME-goto-services}
 
 STAGE=${STAGE-dev}
 STAGE=$(echo $STAGE | tr '[A-Z]' '[a-z]')
-DEPLOY_DIR="vagrant@12.12.12.12:/$STAGE"
+DEPLOY_DIR="vagrant@12.12.12.12/environment/$STAGE"
 
 echo "create dist-$SERVICE_NAME.zip"
-chmod +x ./dist/scripts/*.sh
-zip dist-$SERVICE_NAME.zip ./dist
+cd ./dist
+chmod +x ./scripts/*.sh
+zip dist-$SERVICE_NAME.zip .
 
 echo "deploy dist-$SERVICE_NAME.zip on stage $STAGE"
 scp -i /root/.ssh/id_rsa -o "StrictHostKeyChecking no" dist-$SERVICE_NAME.zip $DEPLOY_DIR
