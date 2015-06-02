acd3023ca8bddf5fac68841ed6566d688d093d8b
tobi vogel <tvogel@thoughtworks.com>
2015-06-02 23:35:00 +0200

    add missing colon after remote host

--- src/dist/scripts/deploy.sh
+++ src/dist/scripts/deploy.sh
@@ -4,7 +4,7 @@ SERVICE_NAME=${SERVICE_NAME-goto-services}
 
 STAGE=${STAGE-dev}
 STAGE=$(echo $STAGE | tr '[A-Z]' '[a-z]')
-DEPLOY_DIR="vagrant@12.12.12.12/environment/$STAGE"
+DEPLOY_DIR="vagrant@12.12.12.12:/environment/$STAGE"
 
 echo "create dist-$SERVICE_NAME.zip"
 cd ./dist
