c43b9773cd74645b53b8bd3c0e6a44954bfa8f90
tobi vogel <tvogel@thoughtworks.com>
2015-06-02 23:12:28 +0200

    add missing -r flag on zipping current dir

--- src/dist/scripts/deploy.sh
+++ src/dist/scripts/deploy.sh
@@ -9,7 +9,7 @@ DEPLOY_DIR="vagrant@12.12.12.12/environment/$STAGE"
 echo "create dist-$SERVICE_NAME.zip"
 cd ./dist
 chmod +x ./scripts/*.sh
-zip dist-$SERVICE_NAME.zip .
+zip -r dist-$SERVICE_NAME.zip .
 
 echo "deploy dist-$SERVICE_NAME.zip on stage $STAGE"
 scp -i /root/.ssh/id_rsa -o "StrictHostKeyChecking no" dist-$SERVICE_NAME.zip $DEPLOY_DIR
